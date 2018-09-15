c2858bff77a5be5c567da56842ac693b5dee7537
Alec Doran-Twyford <alec@alectronic.co>
2018-09-15 23:53:57 +0100

    building a demo canvas

---
+++ src/app/components/gov-uk/demo-page/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class DemoPage extends React.Component {
+  render() {
+    return (
+      <div className="bob">
+        <header className="bob-header">
+          <h1 className="bob-title">Bob</h1>
+        </header>
+        <p className="bob-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default DemoPage;
