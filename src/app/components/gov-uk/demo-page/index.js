7402ec2f3143dcad15c2abe29188e790045505ed
Alec Doran-Twyford <alec@alectronic.co>
2018-09-16 00:10:25 +0100

    GDS asset for demo

--- src/app/components/gov-uk/demo-page/index.js
+++ src/app/components/gov-uk/demo-page/index.js
@@ -3,13 +3,37 @@ import React from 'react';
 class DemoPage extends React.Component {
   render() {
     return (
-      <div className="bob">
-        <header className="bob-header">
-          <h1 className="bob-title">Bob</h1>
-        </header>
-        <p className="bob-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="govuk-width-container">
+        <main className="govuk-main-wrapper govuk-main-wrapper--l" id="main-content" role="main">
+          <div className="govuk-grid-row">
+            <div className="govuk-grid-column">
+              <div className="govuk-panel govuk-panel--confirmation">
+                <h1 className="govuk-panel__title">
+                  Application complete
+                </h1>
+                <div className="govuk-panel__body">
+                  Your reference number<br/><strong>HDJ2123F</strong>
+                </div>
+              </div>
+
+
+              <p className="govuk-body">We have sent you a confirmation email.</p>
+
+              <h3 className="govuk-heading-m">What happens next</h3>
+
+              <p className="govuk-body">
+                We’ve sent your application to Hackney Electoral Register Office.
+              </p>
+              <p className="govuk-body">
+                They will contact you either to confirm your registration, or to ask for more information.
+              </p>
+
+              <p className="govuk-body">
+                <a href="#" className="govuk-link">What did you think of this service?</a> (takes 30 seconds)
+              </p>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
